5d323084808233689324870279245ab425e65564
Anderson Moreira <anderson@afgsolucoes.com.br>
2026-06-13 00:52:57 -0300

    auto: update stats.js

--- backend/src/routes/stats.js
+++ backend/src/routes/stats.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const { getStats } = require('../controllers/stats');
-const auth = require('../middleware/auth');
 
-router.get('/', auth, getStats);
+router.get('/', getStats);
 
 module.exports = router;
